82dcafdda785a1418a865bef4702adce66948935
moyaverdugo <moyaverdugo@gmail.com>
2024-10-19 18:28:04 -0700

    Completed 100% functionality by adding the publish button functionality
    I added the notification and navigation back to the homepage, plus some extra functionality to the Cancel button. Now, I am missing the diving deeper section only.

--- project/src/components/uploadPage/UploadPage.jsx
+++ project/src/components/uploadPage/UploadPage.jsx
@@ -1,10 +1,38 @@
-
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import Header from "../header/Header";
 import './uploadPage.css';
 import heroImage from '../../assets/images/Upload-video-preview.jpg'
 import uploadIcon from '../../assets/icons/upload.svg'
 
 function UploadPage() {
+
+  // THIS IS OUT OF THE SCOPE OF THE ASSIGNMENT.... JUST TRYING TO SEE WHETHER I CAN DO SOMETHING INTERESTING HERE ////////////////////////////////////
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const navigate = useNavigate();
+  /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+  const handlePublish = () => {
+    if (title && description) {
+      // Notification
+      alert(`Wohoo your video "${title}" was uploaded successfully!!`); // This is within the scope. I just thought, since I have to do this i might try something with the title and description.
+
+      // Redirect to the Home Page. Also part of the assignment
+      navigate('/', { state: { selectedVideo: 'default' } }); // I assume BrainFlix 3 will include something like this and adding the video to some kind of storage.
+    } else {
+      // Notify user if form is incomplete
+      alert('Incomplete Form');
+    }
+  };
+
+  // OUT OF THE SCOPE OF THE ASSIGNMENT //////////////////////////////////////////////////////////////////////////////////////////////
+  const handleCancel = () => {
+    setTitle('');
+    setDescription('');
+  };
+  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
   return (
     <>
       <Header />
@@ -17,30 +45,30 @@ function UploadPage() {
         <div className="upload__middle">
           <div className="upload__img_container">
             <h3 className="silver__font"> VIDEO THUMBNAIL</h3>
-            <img className="upload__img" src={heroImage} />
+            <img className="upload__img" src={heroImage} alt="Video Thumbnail"/>
           </div>
           <div className="upload__video">
             <div className="upload__title_video">
                 <label>  
                   <h4 className="silver__font"> TITLE YOUR VIDEO </h4>
                 </label>
-                <input className="upload__form_title form" type="text" name="search" id="search" placeholder="Add a title to your video"/>
+                <input className="upload__form_title form" type="text" name="search" id="search" placeholder="Add a title to your video" value={title} onChange={(e) => setTitle(e.target.value)}/>
             </div>
             <div className="upload__description_video">
               <label>
                 <h4 className="silver__font"> ADD A VIDEO DESCRIPTION </h4>
               </label>
-              <input className="upload__form_description form" type="text" name="search" id="search" placeholder="Add a description to your video"/>
+              <input className="upload__form_description form" type="text" name="search" id="search" placeholder="Add a description to your video" value={description} onChange={(e) => setDescription(e.target.value)}/>
             </div>
           </div>  
         </div>
 
         <div className="upload__buttons">
-          <button className="button__container" type="button">
+          <button className="button__container" type="button" onClick={handlePublish}> 
               <img src={uploadIcon} alt="Upload Icon" className="button__icon" />
               <h5 className="button__text">PUBLISH</h5>
           </button>
-          <button className="button__cancel" type="button">
+          <button className="button__cancel" type="button" onClick={handleCancel}> 
               <h5 className="button__cancel_text">CANCEL</h5>
           </button>
         </div>
